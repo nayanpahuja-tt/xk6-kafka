558eda7191c15dceeddd207593cd2d6f3606cfa8
Nayan Pahuja <nayan.pahuja@triotech.co.in>
2026-06-02 13:59:29 +0530

    my github ref
    Signed-off-by: Nayan Pahuja <nayan.pahuja@triotech.co.in>

--- compat.go
+++ compat.go
@@ -1,6 +1,6 @@
 package kafka
 
 import (
-	// Keep root import path compatibility for xk6 --with github.com/mostafa/xk6-kafka/v2.
-	_ "github.com/mostafa/xk6-kafka/v2/pkg/kafka"
+	// Keep root import path compatibility for xk6 --with  github.com/nayanpahuja-tt/xk6-kafka/v2.
+	_ " github.com/nayanpahuja-tt/xk6-kafka/v2/pkg/kafka"
 )
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/mostafa/xk6-kafka/v2
+module github.com/nayanpahuja-tt/xk6-kafka/v2
 
 go 1.26
 
