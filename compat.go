c8c29421fe4ac9358d06102718fcd40709bc6e58
Nayan Pahuja <nayan.pahuja@triotech.co.in>
2026-06-02 14:11:05 +0530

    fix
    Signed-off-by: Nayan Pahuja <nayan.pahuja@triotech.co.in>

--- compat.go
+++ compat.go
@@ -2,5 +2,5 @@ package kafka
 
 import (
 	// Keep root import path compatibility for xk6 --with  github.com/nayanpahuja-tt/xk6-kafka/v2.
-	_ " github.com/nayanpahuja-tt/xk6-kafka/v2/pkg/kafka"
+	_ "github.com/nayanpahuja-tt/xk6-kafka/v2/pkg/kafka"
 )
